a8128c9f6c0a759c89ab90c86a5ab206f0bbea29
xueqian <weijiagou@gmail.com>
2022-07-28 18:09:16 +0800

    bootstrap webpack

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 3000,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, '.src/'),
+    },
+  },
+}
